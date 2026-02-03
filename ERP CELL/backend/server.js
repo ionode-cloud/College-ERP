a3ffffbf85bb7db63c7abd4c2d61e96c988175f1
Pupunfzx <beheraj542@gmail.com>
2026-02-03 16:13:04 +0530

    separate code

--- ERP CELL/backend/server.js
+++ ERP CELL/backend/server.js
@@ -27,8 +27,6 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
-// Handle preflight explicitly
-app.options('*', cors());
 
 app.use(express.json({ limit: '10mb' }));
 app.use(cookieParser());
